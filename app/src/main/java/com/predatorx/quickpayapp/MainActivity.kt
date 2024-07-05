c9cda054e1f1ef46e2b4337151320d78b58bb92f
AJArmaan47 <ajakaarmaan47@gmail.com>
2024-07-05 22:12:19 +0530

    SignUpPage

--- app/src/main/java/com/predatorx/quickpayapp/MainActivity.kt
+++ app/src/main/java/com/predatorx/quickpayapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.predatorx.quickpayapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,15 +12,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.predatorx.quickpayapp.pages.LoginPageLayout
+import com.predatorx.quickpayapp.pages.SignUpPageLayout
 import com.predatorx.quickpayapp.ui.theme.QuickPayAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             QuickPayAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    LoginPageLayout()
+                    SignUpPageLayout()
 
                 }
             }
--- app/src/main/java/com/predatorx/quickpayapp/pages/SignUpScreen.kt
+++ app/src/main/java/com/predatorx/quickpayapp/pages/SignUpScreen.kt
@@ -1,14 +1,31 @@
 package com.predatorx.quickpayapp.pages
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.predatorx.quickpayapp.R
+import java.time.format.TextStyle
 
 @Composable
 fun SignUpPageLayout(){
@@ -19,8 +36,27 @@ fun SignUpPageLayout(){
     var password by remember { mutableStateOf("") }
     var con_pass by remember { mutableStateOf ("") }
 
-    Column {
-        TextField(value = userName, onValueChange = {userName = it})
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(colorResource(R.color.baby_blue)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painterResource(R.drawable.quickpay_logo), null)
+        Spacer(Modifier.padding(top = 10.dp, bottom = 15.dp))
+        Text(text = "SignUp Now", style = androidx.compose.ui.text.TextStyle( fontSize = 30.sp , color = Color.Black , fontWeight = FontWeight.Bold ))
+        Spacer(Modifier.padding(top = 15.dp, bottom = 15.dp))
+        TextField(value = userName, onValueChange = {userName = it}, label = { Text("Enter UserName:") })
+        Spacer(Modifier.padding(top = 15.dp, bottom = 15.dp))
+        TextField(value = number, onValueChange = {number = it}, label = { Text("Enter Contact Number")})
+        Spacer(Modifier.padding(top = 15.dp, bottom = 15.dp))
+        TextField(value = email, onValueChange = {email = it}, label = { Text("Enter Email")})
+        Spacer(Modifier.padding(top = 15.dp, bottom = 15.dp))
+        TextField(value = password, onValueChange = {password = it},  label = { Text("Enter Passsword")})
+        Spacer(Modifier.padding(top = 15.dp, bottom = 15.dp))
+        TextField(value = con_pass, onValueChange = {con_pass = it}, label = { Text("Enter Confirm Password")})
+        Spacer(Modifier.padding(top = 15.dp, bottom = 15.dp))
+
+
     }
 
 }
