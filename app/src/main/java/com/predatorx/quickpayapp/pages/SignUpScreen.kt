f0159c3a369f650a33bb7183ac174252ce22f4f6
AJArmaan47 <ajakaarmaan47@gmail.com>
2024-07-05 20:58:54 +0530

    SignUpPage

--- app/src/main/java/com/predatorx/quickpayapp/pages/SignUpScreen.kt
+++ app/src/main/java/com/predatorx/quickpayapp/pages/SignUpScreen.kt
@@ -1,9 +1,34 @@
 package com.predatorx.quickpayapp.pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+
 
 @Composable
 fun SignUpPageLayout(){
 
+    var userName by remember { mutableStateOf("")}
+    var number by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var con_pass by remember { mutableStateOf ("") }
 
+    Column {
+        TextField(value = userName, onValueChange = {userName = it})
+    }
+
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SignUpScreenPreview(){
+    SignUpPageLayout()
 }
